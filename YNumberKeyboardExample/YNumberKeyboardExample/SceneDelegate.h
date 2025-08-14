6718d4674cfe7b188675c8eab0d75dadee2278aa
zyb <zouyibin@longshine.com>
2025-08-14 11:55:27 +0800

    add example

---
+++ YNumberKeyboardExample/YNumberKeyboardExample/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  YNumberKeyboardExample
+//
+//  Created by ls on 2025/8/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ YNumberKeyboardExample/YNumberKeyboardExample/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  YNumberKeyboardExample
+//
+//  Created by ls on 2025/8/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
